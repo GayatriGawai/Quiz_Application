175ceddd4f8be1eb77977cb2106d1f63a5e47b27
insnapsys <gayatri.g.insynapsys@gmail.com>
2023-11-06 17:04:55 +0530

    Removed array and alertBox as per Shailesh sir

--- app.js
+++ app.js
@@ -10,93 +10,20 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: 'What is the capital of France?',
-        choice1: 'Berlin',
-        choice2: 'Madrid',
-        choice3: 'Paris',
-        choice4: 'Rome',
-        answer: 3,
-    },
-    {
-        question: 'Which planet is known as the Red Planet??',
-        choice1: 'Venus',
-        choice2: 'Jupiter',
-        choice3: 'Mars',
-        choice4: 'Saturn',
-        answer: 3,
-    },
-    {
-        question: 'Who wrote the play "Romeo and Juliet"?',
-        choice1: 'Charles Dickens',
-        choice2: 'William Shakespeare',
-        choice3: 'Jane Austen',
-        choice4: 'Mark Twain',
-        answer: 2,
-    },
-    {
-        question: 'What is the chemical symbol for water?',
-        choice1: 'Wo',
-        choice2: 'H2O',
-        choice3: 'Co',
-        choice4: 'O2',
-        answer: 2,
-    },
-    {
-        question:
-            'Which gas do plants absorb from the atmosphere and use during photosynthesis?',
-        choice1: 'Oxygen',
-        choice2: 'Carbon dioxide',
-        choice3: 'Nitrogen',
-        choice4: 'Hydrogen',
-        answer: 2,
-    },
-    {
-        question: 'What is the largest organ in the human body?',
-        choice1: 'Brain',
-        choice2: 'Heart',
-        choice3: 'Skin',
-        choice4: 'Liver',
-        answer: 3,
-    },
-    {
-        question:
-            'If a triangle has a right angle (90 degrees), what is the name of the longest side?',
-        choice1: 'Hypotenuse',
-        choice2: 'Adjacent',
-        choice3: 'Diagonal',
-        choice4: 'Opposite',
-        answer: 1,
-    },
-    {
-        question: 'What does the HTML stands for?',
-        choice1: 'Hyperlink and Text Markup Language',
-        choice2: 'Hyper Text Markup Language',
-        choice3: 'High-Level Text Markup Language',
-        choice4: 'Hyper Transfer Markup Language',
-        answer: 2,
-    },
-    {
-        question: 'Which company developed the Python programming language?',
-        choice1: 'Google',
-        choice2: 'Microsoft',
-        choice3: 'Facebook',
-        choice4: 'Python Software Foundation',
-        answer: 4,
-    },
-    {
-        question:
-            'What is the most widely used programming language for web development?',
-        choice1: 'Python',
-        choice2: 'Java',
-        choice3: 'C++',
-        choice4: 'JavaScript',
-        answer: 4,
-    },
-];
+let questions = [];
+
+fetch('questions.json')
+    .then((res) => {
+        return res.json(); //json is the method reads the response's body text and attempts to parse it as JSON
+    })
+    .then((loadedQuestions) => {
+        console.log(loadedQuestions);
+        questions = loadedQuestions;
+        startGame();
+    });
 const CORRECT_BONUS = 1;
 const MAX_QUESTIONS = 10;
+console.log('Stored MAX_QUESTIONS:', MAX_QUESTIONS);
 startGame = () => {
     questionCounter = 0;
     score = 0;
@@ -104,15 +31,14 @@ startGame = () => {
     getNewQuestions();
 };
 getNewQuestions = () => {
-    if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
+    if (!availableQuestions.length || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('recentScore', score);
-
         return window.location.assign('/end.html');
     }
 
     questionCounter++;
-    progressText.innerText =
-        'Question ' + questionCounter + '/' + MAX_QUESTIONS;
+
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
 
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`; //You used '' that's why it wasn't working always use `` for the expression
 
@@ -121,33 +47,33 @@ getNewQuestions = () => {
     question.innerText = currentQuestion.question;
 
     choices.forEach((choice) => {
-        const number = choice.dataset['number'];
+        const number = choice.dataset['number']; // study more on this
         choice.innerText = currentQuestion['choice' + number];
     });
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+    availableQuestions.splice(questionIndex, 1); // study more on this. Alternative you can use filter if you don't know the index number
+    // acceptingAnswers = true;
 };
 choices.forEach((choice) => {
     choice.addEventListener('click', (e) => {
-        if (!acceptingAnswers) return;
-
-        acceptingAnswers = false;
+        // if (!acceptingAnswers) return;
+        //  acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
-        let classToApply = 'incorrect';
-        if (selectedAnswer == currentQuestion.answer) {
-            classToApply = 'correct';
-        }
-        if (classToApply === 'correct') {
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestions();
+        }, 1000);
+        if (classToApply == 'correct') {
             incrementScore(CORRECT_BONUS);
         }
-        selectedChoice.parentElement.classList.add(classToApply);
-        alert('You are anwser is ' + classToApply);
-        getNewQuestions();
     });
 });
 incrementScore = (num) => {
     score += num;
     scoreText.innerHTML = score;
 };
-startGame();
+// startGame();
--- end.js
+++ end.js
@@ -1,4 +1,7 @@
 const finalScore = document.getElementById('finalScore');
 const recentScore = localStorage.getItem('recentScore');
+const MAX_QUESTIONS = localStorage.getItem('MAX_QUESTIONS');
 
-finalScore.innerHTML = recentScore;
+console.log('Stored MAX_QUESTIONS:', MAX_QUESTIONS);
+
+finalScore.innerHTML = `${recentScore}/10`;
