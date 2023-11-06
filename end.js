f4799f62f1b7643036c590e019040608b071d3de
insnapsys <gayatri.g.insynapsys@gmail.com>
2023-11-06 17:48:52 +0530

    Added comments as per the grades

--- end.js
+++ end.js
@@ -8,3 +8,22 @@ finalScore.innerHTML = `${recentScore}/${MAX_QUESTIONS} <br>(${(
     (recentScore / MAX_QUESTIONS) *
     100
 ).toFixed(2)}%)`;
+
+const percentageScore = (recentScore / MAX_QUESTIONS) * 100;
+let grade = '';
+if (percentageScore >= 90) {
+    grade = 'Great!';
+} else if (percentageScore >= 70) {
+    grade = 'Good!';
+} else if (percentageScore >= 50) {
+    grade = 'Not bad';
+} else if (percentageScore >= 35) {
+    grade = 'You need practice';
+} else {
+    grade = 'F';
+}
+
+// Display the grade
+const gradeText = document.createElement('p');
+gradeText.textContent = `Grade: ${grade}`;
+finalScore.appendChild(gradeText);
